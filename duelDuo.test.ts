0301d6a0cbff06a0a4ce5293ecf7f8529fec326a
Caitlin Thompson <ctt00297@gmail.com>
2021-12-03 12:07:03 -0600

    part 4 completed

--- duelDuo.test.ts
+++ duelDuo.test.ts
@@ -18,3 +18,19 @@ test('Title shows up when page loads', async () => {
     const displayed = await title.isDisplayed()
     expect(displayed).toBe(true)
 })
+
+test ("draw button", async () => {
+    await driver.sleep(2000)
+
+    const drawBtn = await driver.findElement(By.xpath("(//section/div)[1]")
+
+    await drawBtn.click()
+
+    await driver.sleep(2000)
+})
+
+test ("add to duo button", async () => {
+    await driver.sleep(2000)
+
+    const addBtn = await driver.findElement(By.xpath(""))
+})
